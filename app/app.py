d5a168311726556cd4fcc013b82ffd06fef0111b
victorqrt <vquerette@gmail.com>
2018-10-22 16:46:44 +0000

    Makefile

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from pymongo import MongoClient
 from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def main_route():
-    return "WIP"
+    return "WIPi"
