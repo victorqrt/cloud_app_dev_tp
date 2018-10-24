a7f1f500251d9803dd58479242aa4ded33517f82
VictorQrt <vquerette@gmail.com>
2018-10-24 16:54:05 +0200

    Adds ObjectId handling for Flask

--- app/mongo_ops.py
+++ app/mongo_ops.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from bson.json_util import dumps, loads
+from bson.objectid import ObjectId
 
 class MongoOps:
 
@@ -8,11 +9,35 @@ class MongoOps:
         # The db build is automated anyway (see ../scripts)
         self.client = MongoClient("mongodb+srv://Gentoo:installgentoo@cloudmlvq-r4zfj.mongodb.net/test")
 
-    def get_db_colls(self, db):
-        return self.client[db].collection_names()
+    def get_db_colls(self):
+        return self.client["imports"].collection_names()
 
-    def get_raw_coll(self, db, coll):
-        res = list(self.client[db][coll].find())
-        for e in res:
-            e.pop("_id")
+    def get_raw_coll(self, coll, pop_oid=False):
+        res = list(self.client["imports"][coll].find())
+        if pop_oid:
+            res = deserialize_oids(res)
         return res
+
+    def get_full_players_documents(self):
+        return deserialize_oids(
+            list(self.client["imports"]["players_teams"].aggregate([{
+                "$lookup": {
+                    "from": "players",
+                    "localField": "playerID",
+                    "foreignField": "bioID",
+                    "as": "player"
+                    }
+                }]))
+            )
+
+def deserialize_oids(document): # Flask cannot serialize "ObjectID" type fields, so we recursively stringify them
+    if isinstance(document, list):
+        for e in document:
+            deserialize_oids(e)
+
+    elif isinstance(document, dict):
+        document["_id"] = str(document["_id"])
+        for k,v in document.items():
+            deserialize_oids(v)
+
+    return document
